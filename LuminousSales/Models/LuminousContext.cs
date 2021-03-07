03740b08bce8cad828798ae2f1c7d7277a9be111
batgo6o <biz.mitko@gmail.com>
2021-03-07 12:14:15 +0200

    Fixed DbContext

--- LuminousSales/Models/LuminousContext.cs
+++ LuminousSales/Models/LuminousContext.cs
@@ -8,16 +8,16 @@ namespace Models
 {
    public class LuminousContext : DbContext
     {
-        public LuminousContext(): base("name=LuminousContext")
-        {
-
-        }
         public DbSet<User> User { get; set; }
         public DbSet<Role> Role { get; set; }
         public DbSet<Permission> Permission { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<Deal> Deal{ get; set; }
         public DbSet<Stock> Stock { get; set; }
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=LuminousSales;Integrated Security=True");
+        }
     }
 }
