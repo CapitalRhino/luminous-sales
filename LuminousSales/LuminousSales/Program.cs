54e64cbb8796c0643f6fbc3b564382cca8114c7e
Aneliya Konarcheva <60808931+annie-prog@users.noreply.github.com>
2021-03-07 09:46:54 +0200

    change

--- LuminousSales/LuminousSales/Program.cs
+++ LuminousSales/LuminousSales/Program.cs
@@ -8,7 +8,7 @@ namespace LuminousSales
         {
             int a = int.Parse(Console.ReadLine());
             Console.WriteLine(a);
-            Console.WriteLine("Jazcurka");
+           
            
         }
     }
