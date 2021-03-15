8a091496f3d21ccc99c650a7a6b40d7ae29310d0
batgo6o <biz.mitko@gmail.com>
2021-03-15 13:43:21 +0200

    Added CredentialChecker

---
+++ LuminousSales/Business/Business/UserManagment/CredentialChecker.cs
@@ -0,0 +1,36 @@
+﻿using Models;
+using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Business.UserManagment
+{
+    public class CredentialChecker
+    {
+        private LuminousContext context;
+        public bool CheckIfUserIsCreated()
+        {
+            using (context = new LuminousContext())
+            {
+                if (context.User.ToList().Any())
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+        public bool CheckPassword(string Password)
+        {
+            using (context = new LuminousContext())
+            {
+                if (context.User.ToList().Exists(user => user.Password == Password))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
